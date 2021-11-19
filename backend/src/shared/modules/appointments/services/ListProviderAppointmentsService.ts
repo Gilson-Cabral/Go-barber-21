db3216a10203025b2fabadfa56e3e16cd3d48f61
Gilson Cabral <69884125+Gilson-Cabral@users.noreply.github.com>
2021-11-18 22:42:57 -0300

    Add files via upload

---
+++ backend/src/shared/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,62 @@
+import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
+
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+/**
+ * [ { day: 1, available }, { day: 1, available,  } ]
+ */
+
+ @injectable()
+ class ListProviderAppointmentsService {
+   constructor(
+     @inject('AppointmentsRepository')
+     private appointmentsRepository: IAppointmentsRepository,
+
+     @inject('CacheProvider')
+     private cacheProvider: ICacheProvider,
+   ) {}
+
+    public async execute({
+        provider_id,
+        day,
+        month,
+        year,
+      }: IRequest): Promise<Appointment[]> {
+        const cachekey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
+
+       let appointments = await this.cacheProvider.recover<Appointment[]>(cachekey,);
+
+        //console.log(cacheData);
+
+        //let appointments = null;
+
+        if (!appointments) {
+          appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+            {
+              provider_id,
+              day,
+              month,
+              year,
+            },
+          );
+
+          //console.log('Buscou do banco');
+
+          await this.cacheProvider.save(cachekey, classToClass(appointments));
+        }
+    
+        return appointments;
+      }
+    }
+    
+    export default ListProviderAppointmentsService;
